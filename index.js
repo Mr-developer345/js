d40a6d1a21753b816919c50df0de6fdf04e983f4
Mr-developer345 <halirajput16@gmail.com>
2025-02-22 23:53:09 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-console.log("hello world")
+console.log('hello world')
 
-var samjho = "han aya"
+// alert(" \n \n  Jhone Doe")
 
-console.log(samjho)
-
-console.log( 'a' >= 'A')
-
-console.log(  "A" >= 'a' )
+// alert
 
 
+var names = "hamadali";
 
-console.log( 'z' >= 'Z')
+console.log(names)
+alert("Hamad Ali")
 
-console.log( 'Z' >= 'z')
+
+var Age = 32;
+
+console.log(Age)
+alert("32 years old")
+
+var course = "Certified Mobile Apllication Development";
+
+console.log(course)
+alert("Certified Mobile Apllication Development")
+
+let word = "PIZZA";
+let output = "";
+
+for (let i = word.length; i > 0; i--){output += word.slice(0, i) + "\n";}
+
+alert(output);
+
+
+var email = "halirajput16@gmail.com";
+
+console.log(email)
+alert("My Email Address is halirajput16@gmail.com")
+
+
+
+var book = "I am trying to learn from the book A smarter way to learn JavaScript";
+
+console.log(book)
+alert("I am trying to learn from the book A smarter way to learn JavaScript")
+
+
+var myString = "════════ஜ۩۞۩ஜ════════";
+
+console.log(myString)
+
+alert(myString);
