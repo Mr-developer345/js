d05bf2837d5bbc44ee619e51d314b591acfa595d
Mr-developer345 <halirajput16@gmail.com>
2025-02-22 23:43:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hello world")
+
+var samjho = "han aya"
+
+console.log(samjho)
+
+console.log( 'a' >= 'A')
+
+console.log(  "A" >= 'a' )
+
+
+
+console.log( 'z' >= 'Z')
+
+console.log( 'Z' >= 'z')
